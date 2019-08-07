e162ebe1985db7899b3de026dbff3e13962535e6
euntas <tavstaus@hanmail.net>
2019-08-07 11:34:17 +0900

    eunji@modify: 배경그림 바꿈. 샘플 캐릭터 이미지 추가

--- nyangcoo/LoginScene.cpp
+++ nyangcoo/LoginScene.cpp
@@ -7,18 +7,18 @@ LoginScene::LoginScene() : Scene()
 
 	bg = new StaticObject();
 	bg->Objtype = eObjectType_BGImage;
-	bg->AssetFileName = TEXT("bg.png");
-	bg->rc = Rect(0, 0, 1080, 1920);
+	bg->AssetFileName = TEXT("bg.jpg");
+	bg->rc = Rect(0, 0, 1173, 660);
 
 	Btn* StartBtn = new Btn();
 	StartBtn->ID = eScene_Game;
 	StartBtn->AssetFileName = TEXT("ed.jpg");
-	StartBtn->rc = Rect(100, 100, 200, 150);
+	StartBtn->rc = Rect(600, 200, 200, 100);
 
 	Btn* ExitBtn = new Btn();
 	ExitBtn->ID = eScene_Exit;
 	ExitBtn->AssetFileName = TEXT("ed.jpg");
-	ExitBtn->rc = Rect(100, 300, 200, 350);
+	ExitBtn->rc = Rect(600, 500, 200, 150);
 
 	infoStaticObj.emplace_back(bg);
 	infoStaticObj.emplace_back(StartBtn);
